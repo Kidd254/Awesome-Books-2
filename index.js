260e0919dece7155fdff3e60a800c36a3efd21db
Kidd254 <lorenkioko@gmail.com>
2023-05-29 19:30:12 +0300

    add remove and add function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const dynamic = document.querySelector('#dynamic');
 const listOfBooks = [{
     title: 'atomic habit',
     author: 'James Clear',
@@ -18,23 +19,48 @@ function addBooks(bookTitle, authorName){
         author,
     };
     listOfBooks.push(tempBook);
-    const addedBooks = document.querySelector('.dynamic');
+    const addedBooks = document.querySelector('.addbook');
 const book = document.createElement('div');
 const title = document.createElement('p');
 title.innerText = tempBook.title;
-book.appendChild(title);
+book.append(title);
 const author = document.createElement('p');
 author.innerText = tempBook.author;
-book.appendChild(author);
+book.append(author);
 const removeBtn = document.createElement('button');
 removeBtn.innerText = 'remove';
-book.appendChild(removeBtn);
+book.append(removeBtn);
 removeBtn.addEventListener('click', ()=>{
     book.style.display= 'none';
 });
 const line = document.createElement('hr');
-book.appendChild(line);
-addedBooks.appendChild(book);
+book.append(line);
+addedBooks.append(book);
 }
 
-
+function removeBook(index) {
+  listOfBooks.splice(listOfBooks.findIndex((e) => e.title === listOfBooks[index].title
+  && e.author === listOfBooks[index].author), 1);
+}
+// Display all books saved in the collection in the top part of the page.
+function bookLoders() {
+  for (let k = 0; k < listOfBooks.length; k += 1) {
+    const book = document.createElement('article');
+    const title = document.createElement('p');
+    title.innerText = listOfBooks[k].title;
+    const author = document.createElement('p');
+    author.innerText = listOfBooks[k].author;
+    book.append(title);
+    book.append(author);
+    const button = document.createElement('button');
+    button.innerText = 'Remove';
+    button.addEventListener('click', () => {
+      removeBook(k);
+      book.style.display = 'none';
+    });
+    book.append(button);
+    const line = document.createElement('hr');
+    book.append(line);
+    dynamic.append(book);
+  }
+}
